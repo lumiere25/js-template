56caa7d214cacc5a2b02692e0e4bb8c2746e1b5a
lumiere25 <57967828+lumiere25@users.noreply.github.com>
2022-10-08 19:51:34 +1100

    Some more updated changes

---
+++ js/main.js
@@ -0,0 +1,32 @@
+console.log("Hello" );
+//This is an alert for when the codingBoss funtion is called
+function codingBoss() {
+  // alert("hey you!");
+  document.querySelector('.main_contents').style.color = "hotpink";
+}
+
+codingBoss();
+
+
+let firstPrize = 299;
+const companyName = "clg";
+
+let greeting = "Hello";
+let secondPrize = "299.99";
+
+let morning = true;
+
+console.log(morning);
+
+let firstWinner = "Grace";
+console.log("The winner is " + firstWinner);
+
+console.log(`The winner is still ${firstWinner}`);
+
+let winnerAnnouncement = `The winner is still ${firstWinner}`;
+
+let result  = winnerAnnouncement.toUpperCase();
+console.log(`${result.length}`);
+
+let resultLength = result.length;
+console.log(resultLength);
